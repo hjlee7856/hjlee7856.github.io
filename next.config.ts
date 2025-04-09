81c92e49a47d956af740ce5982a32901c1df9ecb
HyeoungJun <hyeongjun@daily-funding.com>
2025-04-09 16:34:09 +0900

    hello world!

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,9 @@ const nextConfig: NextConfig = {
   output: "export", // 정적 HTML로 변환
   basePath: "", // 퍼스널 페이지용 → 빈 문자열
   trailingSlash: true, // GitHub Pages 호환
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
