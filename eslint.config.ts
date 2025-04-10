3dea52c8d3fd6c3a9a1ea90365aeb2cf24f504ec
HyeoungJun <hyeongjun@daily-funding.com>
2025-04-10 10:18:42 +0900

    폰트 cdn -> 로컬로 변경

---
+++ eslint.config.ts
@@ -0,0 +1,14 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'prettier'],
+  extends: [
+    'next',
+    'next/core-web-vitals',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  rules: {
+    'prettier/prettier': 'error',
+  },
+};
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,12 +3,7 @@ import { Head, Html, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html lang="ko">
-      <Head>
-        <link
-          rel="stylesheet"
-          href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard/dist/web/variable/pretendardvariable-dynamic-subset.css"
-        />
-      </Head>
+      <Head></Head>
       <body className="antialiased">
         <Main />
         <NextScript />
