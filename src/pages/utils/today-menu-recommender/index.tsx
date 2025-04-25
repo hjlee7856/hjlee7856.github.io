c0804c5260d6bc248e575ddcdd09b54f95ccc164
HyeoungJun <hyeongjun@daily-funding.com>
2025-04-25 09:53:22 +0900

    임시 수정

--- src/pages/utils/today-menu-recommender/index.tsx
+++ src/pages/utils/today-menu-recommender/index.tsx
@@ -1,12 +1,9 @@
-import NaverMap from '@/components/naverMap';
 import { Box } from '@mui/material';
 
 const TodayMenuRecommender = () => {
   return (
     <Box p={2}>
-      <Box width={'500px'}>
-        <NaverMap destinationAddress={'도곡길 27'} />
-      </Box>
+      <Box width={'500px'}>{/* <NaverMap destinationAddress={'도곡길 27'} /> */}</Box>
     </Box>
   );
 };
